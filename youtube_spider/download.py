bb141cfcece8f8c250152100382b3e1f08ab5295
jaluik <cqzhengmouren@gmail.com>
2020-12-25 15:41:27 +0800

    feat: 调整容错处理

--- youtube_spider/download.py
+++ youtube_spider/download.py
@@ -1,37 +1,32 @@
-import requests
-import time
 import os
+import sys
 import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-
 
 
 class YoutubeSpider():
     num = 1
-    file_name = "./href.txt"
     driver = None
-    fd=None
+    fd = None
     youtube_down_list = []
 
     def main(self):
         option = webdriver.ChromeOptions()
         option.add_argument('headless')
-        self.driver = webdriver.Chrome("./chromedriver")
+        self.driver = webdriver.Chrome(sys.path[0] + "/chromedriver")
         try:
             self.get_youtube_url()
-            # self.get_download_url()
+            self.get_download_url()
             print(self.youtube_down_list)
         finally:
             self.driver.close()
 
-
-    def get_youtube_url(self): 
-        save_name = "youtube.npy"
-        if(os.path.exists(save_name)):
+    def get_youtube_url(self):
+        save_name = sys.path[0] + "/youtube.npy"
+        if os.path.exists(save_name):
             youtube = np.load(save_name)
             self.youtube_down_list = youtube.tolist()
             return
@@ -45,14 +40,16 @@ class YoutubeSpider():
             self.youtube_down_list.append(str(youtube_down))
         youtube = np.array(self.youtube_down_list)
         np.save(save_name, youtube)
-        
 
     def get_download_url(self):
-        fd = open(self.file_name, "w")
-        num= 1
+        file_name = sys.path[0] + "/href.txt"
+        file_err_name = sys.path[0] + "/href.txt"
+        fd = open(file_name, "w")
+        fe = open(file_err_name, "w")
+        num = 1
         base_url = "https://zh.savefrom.net/7/"
         self.driver.get(base_url)
-        for youtube_url in self.youtube_down_list: 
+        for youtube_url in self.youtube_down_list:
             inputs = self.driver.find_elements_by_css_selector('.tarea-wrap > input')
             for input in inputs:
                 input.send_keys(youtube_url)
@@ -61,16 +58,26 @@ class YoutubeSpider():
                 btn.click()
                 input.clear()
             # 等到元素出现后再显示
-            elem = WebDriverWait(self.driver, ).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.def-btn-box > a")))
-            print(elem)
-            name = elem.get_attribute("download")
-            href = elem.get_attribute("href")
-            text = f"{href}\r\n"
-            print(f"正在写入{num}个文件： {name}")
-            fd.write(text)
-            num += 1
+            try:
+                elem = WebDriverWait(self.driver, 15).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, "div.def-btn-box > a")))
+                print(elem)
+                name = elem.get_attribute("download")
+                href = elem.get_attribute("href")
+                text = f"{href}\r\n"
+                print(f"正在写入{num}个文件： {name}")
+                fd.write(text)
+                num += 1
+            except:
+                print("某个文件出问题啦")
+                print("未能下载的url:", youtube_url)
+                fe.write(youtube_url)
+                continue
+
         print(f"{num - 1}个文件地址写入成功！")
         fd.close()
+        fe.close()
+
 
 if __name__ == "__main__":
     spider = YoutubeSpider()
